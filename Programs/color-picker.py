0ed05861659639722bace88ffcc4a507fc8c33bd
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2020-11-21 18:56:22 +0530

    Update color-picker.py

--- Programs/color-picker.py
+++ Programs/color-picker.py
@@ -2,16 +2,26 @@ import tkinter as tk
 from tkinter import colorchooser
 
 root=tk.Tk()
+root.title("Color Picker")
+root.configure(bg='black')
+label = tk.Label(root,
+                text="Pick the color",
+                fg="white",
+                bg="black",
+                width=30,
+                height=5,
+                font=("Courier", 30))
+label.pack()
+
 color=colorchooser.askcolor()
 
 print(color[1])
 
 label = tk.Label(root,
-                text="You picked "+color[1],
+                text="Hex code of color you picked "+color[1],
                 fg="white",
                 bg="black",
-                width=30,
-                height=10,
-                font=("Courier", 30))
+                height=5,
+                font=("Courier", 20))
 label.pack()
 tk.mainloop()
