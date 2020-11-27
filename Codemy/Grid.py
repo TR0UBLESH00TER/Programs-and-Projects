fcefddd303c02de1d4734552c2677762fddbf8ad
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2020-11-27 19:40:52 +0530

    Program 1

--- Codemy/Grid.py
+++ Codemy/Grid.py
@@ -3,11 +3,17 @@ from tkinter import *
 root = Tk()
 
 #Creating a Label Widget
-myLabel1 = label(root, text = "Hello World!").grid(row=0, column=0)
-myLabel2 = label(root, text = "Hello World!").grid(row=1, column=1)
+Label(root, text = "Hello World!").grid(row=0, column=0)
+Label(root, text = "~Tkinter").grid(row=1, column=1)
 
-#Showing it onto the screen
-myLabel1.pack()
-myLabel2.pack()
+'''
+OR
+#Creating a Label Widget
+myLabel1 = Label(root, text = "Hello World!")
+myLabel2 = (root, text = "~Tkinter")
+
+myLabel2.grid(row=0, column=0)
+myLabel2.grid(row=1, column=1)
+'''
 
 root.mainloop()
