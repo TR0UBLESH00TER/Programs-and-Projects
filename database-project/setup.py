3c9a9c93251a08654ec82d74cd8e4f3e6384731d
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2021-01-06 12:19:02 +0530

    Statement to check if already file run
    Added statement to check if setup.py was already run and display message otherwise it will continue to the program.

--- database-project/setup.py
+++ database-project/setup.py
@@ -10,12 +10,49 @@ from tkinter import *
 from tkinter import messagebox
 import mysql.connector
 
+# Checking if setup.py was already run or not and display message to run main.py if it was already run.
+try:
+    with open(".\\data\\data.txt","r") as f:
+        messagebox.showinfo("Alert","The setup was already completed. Please use \"main.py\"")
+        quit()    # Closes the wndow
+except Exception:
+    # The code will continue
+    f = ""
+
 #Creating the window
 login = Tk()
 login.title(" Login")
-login.iconbitmap(".\\ico\\Data.ico")
+login.iconbitmap(".\\data\\Data.ico")
 login.geometry("600x400")
 login.resizable(0,0)
+
+def password():          # Function to accept & store password and create database "school_data"
+    global pwd
+    global frame
+    global login
+    pwd = e.get()
+    try:
+        db=mysql.connector.connect(host='localhost', user='root', passwd=pwd)
+        if db.is_connected():
+            
+            c=db.cursor()
+            c.execute("CREATE DATABASE school_data;")
+
+            # Storing the password in a seperate file for later use
+            with open(".\\data\\data.txt","w") as f:
+                f.write(pwd)
+
+            # Displaying success message
+            messagebox.showinfo("Success","Successfully logged in and the database named \"school_data\" was created.")
+
+            frame.destroy()
+            login.title(" Setup")
+            frame = LabelFrame(login,text="Setup",padx=20,pady=100,labelanchor=N)
+            frame.pack(padx=20,pady=20)
+            Button(frame,text="Create Tables",padx=40,pady=20,command=createtable).grid(row=0,column=0,padx=40,pady=40)
+    except Exception:
+        messagebox.showerror("Invalid Password","The password you have entered is incorrect.")
+        e.delete(0, END) # Clearing the Entry box
 
 def createtable():         # Function to create table in MySQL
     global pwd
@@ -34,33 +71,6 @@ def createtable():         # Function to create table in MySQL
 
     # Closing the window after completing the setup
     login.destroy()
-
-def password():          # Function to accept & store password and create database "school_data"
-    global pwd
-    global frame
-    global login
-    pwd = e.get()
-    try:
-        db=mysql.connector.connect(host='localhost', user='root', passwd=pwd)
-        if db.is_connected():
-            # Storing the password in a seperate file for later use
-            with open("data.txt","w") as f:
-                f.write(pwd)
-            
-            c=db.cursor()
-            c.execute("CREATE DATABASE school_data;")
-
-            # Displaying success message
-            messagebox.showinfo("Success","Successfully logged in and the database named \"school_data\" was created.")
-
-            frame.destroy()
-            login.title(" Setup")
-            frame = LabelFrame(login,text="Setup",padx=20,pady=100,labelanchor=N)
-            frame.pack(padx=20,pady=20)
-            Button(frame,text="Create Tables",padx=40,pady=20,command=createtable).grid(row=0,column=0,padx=40,pady=40)
-    except Exception:
-        messagebox.showerror("Invalid Password","The password you have entered is incorrect.")
-        e.delete(0, END) # Clearing the Entry box
 
 # Creating frame for login
 frame = LabelFrame(login,text="Setup",padx=20,pady=100,labelanchor=N)
