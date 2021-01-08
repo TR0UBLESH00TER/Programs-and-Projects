e9e5abe7a234d8b6c0774ba07032ff186fda129c
TR0UBLESH00TER <74814583+TR0UBLESH00TER@users.noreply.github.com>
2021-01-08 19:04:23 +0530

    Update color-picker.py

--- Programs/color-picker.py
+++ Programs/color-picker.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from tkinter import colorchooser
 
+def _from_rgb(rgb):
+    # Translates an rgb tuple of int to a tkinter friendly color code
+    return "#%02x%02x%02x" % rgb  
+
 root=tk.Tk()
 root.title("Color Picker")
 root.configure(bg='black')
@@ -14,13 +18,16 @@ label = tk.Label(root,
 label.pack()
 
 color=colorchooser.askcolor()
+inv_color = _from_rgb((int(255-color[0][0]),int(255-color[0][1]),int(255-color[0][2])))
+print(color[0])
 
-print(color[1])
+label.config(fg=inv_color, bg=color[1])
+root.configure(bg=color[1])
 
 label = tk.Label(root,
                 text="Hex code of color you picked "+color[1],
-                fg="white",
-                bg="black",
+                fg=inv_color,
+                bg=color[1],
                 height=5,
                 font=("Courier", 20))
 label.pack()
